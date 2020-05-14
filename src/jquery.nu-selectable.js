ee939164e187db3899af8354a0582d2c074f6d83
numpde <21158052+numpde@users.noreply.github.com>
2020-05-14 15:18:46 +0200

    Iteration

--- src/jquery.nu-selectable.js
+++ src/jquery.nu-selectable.js
@@ -2,211 +2,191 @@
  * nuSelectable - jQuery Plugin
  * Copyright (c) 2015, Alex Suyun
  * Copyright (c) 2018, Carl-Philip Hänsch
+ * Copyright (c) 2020, RA
  * Copyrights licensed under The MIT License (MIT)
  */
 ;
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
 
-  'use strict';
+    'use strict';
 
-  var plugin = 'nuSelectable';
+    var plugin = 'nuSelectable';
 
-  var defaults = {
-    onSelect: function() {},
-    onUnSelect: function() {},
-    onClear: function() {},
-    onFinish: function () {}
-  };
+    // Event horizon
+    function blackhole(evt) { evt.stopPropagation(); evt.preventDefault(); }
 
-  var nuSelectable = function(container, options) {
-    this.container = $(container);
-    this.options = $.extend({}, defaults, options);
-    this.selection = $('<div>')
-      .addClass(this.options.selectionClass);
-    this.items = $(this.options.items);
-    this.init();
-  };
+    var noop = function() { }
 
-  nuSelectable.prototype.init = function() {
-    if (!this.options.autoRefresh) {
-      this.itemData = this._cacheItemData();
-    }
-    this.selecting = false;
-    this._normalizeContainer();
-    this._bindEvents();
-    return true;
-  };
+    var defaults = {
+        start: noop,
+        select: noop,
+        unselect: noop,
+        stop: noop,
 
-  nuSelectable.prototype._normalizeContainer = function() {
-    this.container.css({
-      '-webkit-touch-callout': 'none',
-      '-webkit-user-select': 'none',
-      '-khtml-user-select': 'none',
-      '-moz-user-select': 'none',
-      '-ms-user-select': 'none',
-      'user-select': 'none'
-    });
-  };
+        items: undefined,
+        boxClass: undefined,
+        selectedClass: undefined,
 
-  nuSelectable.prototype._cacheItemData = function() {
-    var itemData = [],
-      itemsLength = this.items.length;
+        distance: 10,
+        itemsChange: true,
+    };
 
-    for (var i = 0, item; item = $(this.items[i]), i <
-      itemsLength; i++) {
-      itemData.push({
-        element: item,
-        selected: item.hasClass(this.options.selectedClass),
-        selecting: false,
-        position: item[0].getBoundingClientRect()
-      });
-    }
-    return itemData;
-  };
+    var THIS = function (container, options) {
+        this.container = $(container);
+        this.options = $.extend({}, defaults, options);
+        this.selectionBox = $('<div>').addClass(this.options.boxClass);
+      
+        this.selecting = false;
+        this._normalizeContainer();
+        this._bindEvents();
+        this._cacheItemData();
+    };
 
-  nuSelectable.prototype._collisionDetector = function() {
-    var selector = this.selection[0].getBoundingClientRect(),
-      dataLength = this.itemData.length;
-    // Using native for loop vs $.each for performance (no overhead)
-    for (var i = dataLength - 1, item; item = this.itemData[i], i >=
-      0; i--) {
-      var collided = !(selector.right < item.position.left ||
-        selector.left > item.position.right ||
-        selector.bottom < item.position.top ||
-        selector.top > item.position.bottom);
+    THIS.prototype._normalizeContainer = function () {
+        this.container.css({
+            '-webkit-touch-callout': 'none',
+            '-webkit-user-select': 'none',
+            '-khtml-user-select': 'none',
+            '-moz-user-select': 'none',
+            '-ms-user-select': 'none',
+            'user-select': 'none'
+        });
+    };
 
-      if (collided) {
-        if (item.selected) {
-          item.element.removeClass(this.options.selectedClass);
-          item.selected = false;
+    THIS.prototype._cacheItemData = function () {
+        this.itemData = $(this.options.items).toArray().map(
+            (item, i) => ({
+                element: $(item),
+                selected: $(item).hasClass(this.options.selectedClass),
+                position: item.getBoundingClientRect()
+            })
+        );
+    };
+
+    THIS.prototype._collideWithItems = function () {
+        var selectionBox = this.selectionBox[0].getBoundingClientRect();
+        for (var item of this.itemData) {
+            var collision = !(
+                selectionBox.right < item.position.left ||
+                selectionBox.left > item.position.right ||
+                selectionBox.bottom < item.position.top ||
+                selectionBox.top > item.position.bottom
+            );
+
+            if (collision && !item.selected) {
+                item.selected = true;
+                item.element.addClass(this.options.selectedClass);
+                this.options.select(item.element);
+            }
+            
+            if (item.selected && !collision && this.selecting /* why? */) {
+                item.selected = false;
+                item.element.removeClass(this.options.selectedClass);
+                this.options.unselect(item.element);
+            }
+
         }
-        if (!item.selected) {
-          item.element.addClass(this.options.selectedClass);
-          item.selected = true;
-          this.options.onSelect(item.element);
+    };
+
+    THIS.prototype._createBox = function () {
+        this.selectionBox.appendTo(document.body).css({
+            position: 'absolute',
+            overflow: 'hidden',
+        });
+    };
+
+    THIS.prototype._drawBox = function (css) {
+        this.selectionBox.css(css).css({'z-index': Math.round(Date.now() / 1000)});
+    };
+
+    THIS.prototype._mousedown = function (evt) {
+        if (evt.which !== 1) {
+            return false;
         }
-      }
-      else {
+        if (evt.metaKey || evt.ctrlKey) {
+            return false;
+        }
+        
+        if (!this.options.disable) {
+            // Defer selection: wait to achieve `distance`
+            this.selecting = false;
+            this.pos = [evt.pageX, evt.pageY];
+        }
+    };
+
+    THIS.prototype._mousemove = function (evt) {
+        var pos = this.pos;
+        if (!pos) {
+            // There was no mousedown
+            return false;
+        }
+
+        var newpos = [evt.pageX, evt.pageY];
+        var width = Math.abs(newpos[0] - pos[0]);
+        var height = Math.abs(newpos[1] - pos[1]);
+        
+        if (!this.selecting) {
+            // Initiate the selection?
+            if ((width >= this.options.distance) || (height >= this.options.distance)) {
+                this.selecting = true;
+
+                // Callback
+                this.options.start();
+
+                // Re-collect selectables
+                if (this.options.itemsChange) {
+                    this._cacheItemData();
+                }
+
+                // Add the selection box to DOM
+                this._createBox();
+            }
+        }
+
         if (this.selecting) {
-          item.element.removeClass(this.options.selectedClass);
-          this.options.onUnSelect(item.element);
+            blackhole(evt);            
+            var left = (newpos[0] < pos[0]) ? (pos[0] - width) : pos[0];
+            var top = (newpos[1] < pos[1]) ? (pos[1] - height) : pos[1];
+            this._drawBox({width: width, height: height, top: top, left: left});
+            this._collideWithItems();
         }
-      }
+    };
 
-    }
-  };
+    THIS.prototype._mouseup = function (evt) {
+        // Undo `mousedown`
+        this.pos = undefined;
 
-  nuSelectable.prototype._createSelection = function(x, y) {
-    this.selection.css({
-        'position': 'absolute',
-        'top': y + 'px',
-        'left': x + 'px',
-        'width': '0',
-        'height': '0',
-        'z-index': '999',
-        'overflow': 'hidden'
-      })
-      .appendTo(document.body);
-  };
+        if (this.selecting) {
+            blackhole(evt);
 
-  nuSelectable.prototype._drawSelection = function(width, height, x,
-    y) {
-    this.selection.css({
-      'width': width,
-      'height': height,
-      'top': y,
-      'left': x
-    });
-  };
+            this.selecting = false;
+            this.selectionBox.remove();
 
-  nuSelectable.prototype.clear = function() {
-    this.items.removeClass(this.options.selectedClass);
-    this.options.onClear();
-  };
-
-  nuSelectable.prototype._mouseDown = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    if (this.options.disable) {
-      return false;
-    }
-    if (event.which !== 1) {
-      return false;
-    }
-    if (this.options.autoRefresh) {
-      this.itemData = this._cacheItemData();
-    }
-    if (event.metaKey || event.ctrlKey) {
-      this.selecting = false;
-    }
-    else {
-      this.selecting = true;
-    }
-    this.pos = [event.pageX, event.pageY];
-    this._createSelection(event.pageX, event.pageY);
-
-  };
-
-  nuSelectable.prototype._mouseMove = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    // Save some bytes
-    var pos = this.pos;
-    if (!pos) {
-      return false;
-    }
-    var newpos = [event.pageX, event.pageY],
-      width = Math.abs(newpos[0] - pos[0]),
-      height = Math.abs(newpos[1] - pos[1]),
-      top, left;
-
-    top = (newpos[0] < pos[0]) ? (pos[0] - width) : pos[0];
-    left = (newpos[1] < pos[1]) ? (pos[1] - height) : pos[1];
-    this._drawSelection(width, height, top, left);
-    this._collisionDetector();
-
-  };
-
-  nuSelectable.prototype._mouseUp = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    if (!this.pos) {
-      return false;
-    }
-    this.selecting = false;
-    this.selection.remove();
-    if (event.pageX === this.pos[0] && event.pageY === this.pos[1]) {
-      this.clear();
-    }
-    this.options.onFinish();
-  };
-
-  nuSelectable.prototype._bindEvents = function() {
-    this.container.on('mousedown', $.proxy(this._mouseDown, this));
-    this.container.on('mousemove', $.proxy(this._mouseMove, this));
-    // Binding to document is 'safer' than the container for mouse up
-    $(document)
-      .on('mouseup', $.proxy(this._mouseUp, this));
-  };
-
-  $.fn[plugin] = function(options) {
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    return this.each(function() {
-      var item = $(this),
-        instance = item.data(plugin);
-      if (!instance) {
-        item.data(plugin, new nuSelectable(this, options));
-      }
-      else {
-
-        if (typeof options === 'string' && options[0] !== '_' &&
-          options !== 'init') {
-          instance[options].apply(instance, args);
+            // Callback
+            this.options.stop();
         }
-      }
+    };
 
-    });
-  };
+    THIS.prototype._bindEvents = function () {
+        this.container.on('mousedown', $.proxy(this._mousedown, this));
+        $(document).on('mousemove', $.proxy(this._mousemove, this));
+        // TODO: document may not receive mouseup?
+        $(document).on('mouseup', $.proxy(this._mouseup, this));
+    };
+
+    $.fn[plugin] = function (options, ...args) {
+        return this.each(function () {
+            var item = $(this);
+            var instance = item.data(plugin);
+            if (!instance) {
+                item.data(plugin, new THIS(this, options));
+            } else {
+                if ((typeof options === 'string') && (options[0] !== '_') && (options !== 'init')) {
+                    instance[options].apply(instance, args);
+                }
+            }
+
+        });
+    };
 
 })(jQuery, window, document);
